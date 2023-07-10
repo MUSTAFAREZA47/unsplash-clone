3882adb0d94b0dd301cb1807dfa12d49c1099055
Ahmed Reza <mustafareza47@gmail.com>
2023-07-10 17:52:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photoArray = [];
+
+// Loading
+// function loading() {
+//   loader.hidden = false;
+//   imageContainer.hidden = true;
+// }
+
+// // complete Loading
+// function complete() {
+//   loader.hidden = true;
+//   imageContainer.hidden = false;
+// }
+
+// Unsplash API
+const count = 30;
+const apiKey = "b8HbiEfRxZcXm3AYSQzH3QkWUx3AWesqq8WsuzA-Om0";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create element for links and photo and dom
+function displayPhotos() {
+//   loading();
+  photoArray.forEach((photo) => {
+    // create <a></a> to link to unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+
+    // create <img> for photo
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("atl", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+
+    // put <img> inside <a></a> then put both inside imageContainer elemet
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+    // complete();
+  });
+}
+
+// Get photo from Unsplash API
+async function getPhoto() {
+//   loading();
+  try {
+    const response = await fetch(apiUrl);
+    photoArray = await response.json();
+    // console.log(photoArray);
+    displayPhotos();
+  } catch (error) {
+    console.log("whoopps, Error found --->", error);
+  }
+}
+
+// Check to see if the scrolling near to the bottom of page, Load more photo.
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhoto();
+    console.log("load more");
+  }
+});
+
+// On Load
+getPhoto();
